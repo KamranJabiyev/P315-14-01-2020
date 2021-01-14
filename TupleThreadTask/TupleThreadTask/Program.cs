2f79e18c147498d2d738e20d1189d3bba7059e58
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2021-01-14 21:42:03 +0400

    Add files via upload

---
+++ TupleThreadTask/TupleThreadTask/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TupleThreadTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tuple,ValueTuple
+            #region Tuple-reference
+            //Tuple<int, string, string> person = new Tuple<int, string, string>(1, "Fikret", "Javadov");
+            //var person = Tuple.Create(1, "Fikret", "Jvadov",4,5,6,7,8);
+            //Console.WriteLine(person.Rest.Item1);
+            #endregion
+            #region ValueTuple - value
+            //ValueTuple<int, string, string> person = (1, "Fikret", "Javadov");
+            //(int,string,string) person= (1, "Fikret", "Javadov");
+            //Console.WriteLine(person.Item1);
+            //var person = (Id: 1, Name: "Fikret", Surname: "Javadov");
+            //Console.WriteLine(person.Surname);
+            #endregion
+            #endregion
+
+            #region Thread
+            //Loop1();
+            //Loop2();
+
+            //Thread thread1 = new Thread(Loop1);
+            //Thread thread2 = new Thread(Loop2);
+            //thread1.Start();
+            ////thread1.Join();
+            //thread2.Start();
+            //Console.WriteLine("P315");
+            #endregion
+        }
+        #region Thread
+        static void Loop1()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Thread.Sleep(0);
+                Console.WriteLine($"Loop1: {i}");
+            }
+        }
+
+        static void Loop2()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Thread.Sleep(1);
+                Console.WriteLine($"Loop2: {i}");
+            }
+        }
+        #endregion
+
+        #region Tuple,ValueTuple
+        static (int, string, string) GetPerson()
+        {
+            return (Id: 1, FirstName: "Bill", LastName: "Gates");
+        }
+
+        //static Person GetPerson()
+        //{
+        //    return new Person { Id = 1, Name = "Tural", Surname = "Alesgerov" };
+        //}
+        #endregion
+    }
+
+    #region Tuple,ValueTuple
+    //class Person
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //    public string Surname { get; set; }
+    //}
+    #endregion
+}
---
+++ WindowsFormsApp/WindowsFormsApp/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //button1.Click += delegate (object sender, EventArgs e)
+            //  {
+            //      MessageBox.Show("Salam");
+            //  };
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text =(await Sum()).ToString();
+        }
+
+        private async Task<long> Sum()
+        {
+            long result = 0;
+            var task=Task.Run(() =>
+            {
+                for (long i = 0; i < 1000000000; i++)
+                {
+                    result += i;
+                }
+            });
+            await task;
+            return result;
+        }
+    }
+}
